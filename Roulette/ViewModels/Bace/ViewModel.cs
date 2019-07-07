97a9a8fafac7e36ecb73fdf2c4dbf5d763798e1e
AleksejSinchuk <aleksejsincuk@gmail.com>
2019-07-07 17:19:01 +0300

    MyRoulette

---
+++ Roulette/ViewModels/Bace/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Roulette.ViewModels.Bace
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")//функция обновления конкретного привязаного поля в View
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        public class Command : ICommand
+        {
+            public delegate void Del();
+            Del del;
+            #region ICommand Members  
+            public Command(Del del)
+            {
+                this.del = del;
+            }
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+            public void Execute(object parameter)
+            {
+                del();
+            }
+            #endregion
+        }
+    }
+}
